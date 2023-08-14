1225e7f45b7113933452c00b15c5b6edee672b9b
Rishu Kumar <106136366+rishu0007@users.noreply.github.com>
2023-08-15 00:44:31 +0530

    NodeJs

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+superheroes.all;
+const name = superheroes.random();
+console.log(`My name is ${name}!`);
